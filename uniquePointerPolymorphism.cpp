3a9d7c4df2ae9d239d6c3fc95dc7ccd66d797de6
Saygin Guven <sayginguven@yahoo.com>
2019-02-27 10:29:44 -0800

    id to name and 2 ways to push back

--- uniquePointerPolymorphism.cpp
+++ uniquePointerPolymorphism.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <memory>
 #include <vector>
 
 class Animal { // abstract
@@ -12,57 +13,78 @@ public:
 };
 
 class Cat :public Animal {
-private: 
-	int id;
+private:
+	std::string name;
 
 public:
-	Cat(int id) : id{ id } {}
-	Cat():Cat(0) {}
+	Cat(std::string id) : name{ id } {}
+	Cat() :Cat(0) {}
 
 	void sound() override {
-		std::cout << "my id is "<< id << " and I say meow" << std::endl;
+		std::cout << "my name is " << name << " and I say meow" << std::endl;
 	}
 };
 
 class Dog :public Animal {
 private:
-	int id;
+	std::string id;
 
 public:
-	Dog(int id) : id{ id } {}
+	Dog(std::string id) : id{ id } {}
 	Dog() :Dog(0) {}
 public:
 	void sound() override {
-		std::cout << "my id is " << id << " and I say whoof" << std::endl;
+		std::cout << "my name is " << id << " and I say whoof" << std::endl;
 	}
 };
 
 class Bird :public Animal {
 private:
-	int id;
+	std::string id;
 
 public:
-	Bird(int id) : id{ id } {}
-	Bird() : Bird(0){}
+	Bird(std::string id) : id{ id } {}
+	Bird() : Bird(0) {}
 public:
 	void sound() override {
-		std::cout << "my id is " << id << " and I say tweet" << std::endl;
+		std::cout << "my name is " << id << " and I say tweet" << std::endl;
 	}
 };
 
 
 int main() {
 
-	std::vector<std::unique_ptr<Animal>> Animals;
+	//create vector for Animals1 vector to push back with make unique
+	std::vector<std::unique_ptr<Animal>> Animals1;
 
-	Animals.push_back(std::make_unique<Cat>(1));
-	Animals.push_back(std::make_unique<Dog>(2));
-	Animals.push_back(std::make_unique<Bird>(3));
-
-	for (const auto &animal : Animals) {
+	Animals1.push_back(std::make_unique<Cat>("fluffy"));
+	Animals1.push_back(std::make_unique<Dog>("spike"));
+	Animals1.push_back(std::make_unique<Bird>("dragon"));
+	
+	std::cout << "Animal 1 vector polymorpism" << std::endl;
+	for (const auto &animal : Animals1) {
 		animal->sound();
+		//(*animal).sound();
 	}
 
+	//create vector for Animals2 vector to push back after creating the unique pointer
+	std::vector<std::unique_ptr<Animal>> Animals2;
+
+	std::unique_ptr<Animal> cat1 = std::make_unique<Cat>("twity");
+	std::unique_ptr<Animal> dog1= std::make_unique<Dog>("marley");
+	std::unique_ptr<Animal> bird1 = std::make_unique<Bird>("angry bird");
+
+	Animals2.push_back(std::move(cat1));
+	Animals2.push_back(std::move(dog1));
+	Animals2.push_back(std::move(bird1));
+	
+
+	std::cout << "\n\nAnimal 2 vector polymorpism" << std::endl;
+	for (const auto &animal : Animals2) {
+		animal->sound();
+		//(*animal).sound();
+	}
+	
 
 	system("pause");
 	return 0;
